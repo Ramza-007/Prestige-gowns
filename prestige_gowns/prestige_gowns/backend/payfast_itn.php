995ecdbf28d3dcb03e56fb1362c1fed5c1a7f423
Double-O-Seven <ramzachaane8@gmail.com>
2025-12-24 23:34:39 +0200

    Delete prestige_gowns/prestige_gowns directory

--- prestige_gowns/prestige_gowns/backend/payfast_itn.php
+++
@@ -1,3 +0,0 @@
-<?php
-file_put_contents("payments.log",   json_encode($_post), FILE_APPEND);
-?>
--- prestige_gowns/prestige_gowns/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "prestige_gowns");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
